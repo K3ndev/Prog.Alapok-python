b0b5bd3965446408cde14adbda2273684faa8666
K3dne <kiss-peto.kende@petrik.hu>
2026-09-13 14:23:43 +0200

    finishing the execises to the 4.

---
+++ main.py
@@ -0,0 +1,52 @@
+print("------- 1. feladat --------")
+
+print("hello word!")
+
+
+print("------- 2. feladat --------")
+
+print("isten hozott a python világában!")
+print()
+
+print("\tÜdv: a program")
+print()
+
+print("isten hozott a python világában!\n\n\tÜdv: a program")
+print("""
+isten hozott a python világában!
+
+\tÜdv: a program
+""")
+
+
+print("------- 3. feladat --------")
+
+print('"Ha med tudod álmodni, meg is tudod tenni"', end="\t")
+print("Walt Disney")
+print()
+
+print('"Ha med tudod álmodni, meg is tudod tenni"', end=" - ")
+print("Walt Disney")
+print()
+
+print("Walt Disney:", end="\t")
+print('"Ha med tudod álmodni, meg is tudod tenni"')
+print()
+
+
+print("------- 4. feladat --------")
+
+print("tej", "tej", "tej", "tej", "tej", sep=" ,")
+print("tej", "tej", "tej", "tej", "tej", sep=" | ")
+print("tej", "tej", "tej", "tej", "tej", sep=" *** ")
+
+print("------- 5. feladat --------")
+
+print("""
+\" A 'whitespace' vagy 'white space' angol szóöszetétel, jelentése fehér tér.
+
+Az informatikában, elsősorban a
+\tszövegszerkeztésben és
+\tprogramozásban
+használatos kifejezés
+""")
