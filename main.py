50835153d2baade3a328bdc29788e59905885b03
Kiss-Pető Kende <kiss-peto.kende@petrik.hu>
2026-09-14 12:17:01 +0200

    doing the first task

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ print("----- 1. Feladat -----")
 
 nev = input("Mi a neve? ")
 ev = int(input("Mikor született"))
-print(f"Kendes {nev}! {ev}-ben születtél")
+print(f"Kendes {nev}! {ev}-ben születtél!")
 kor = 2026 - ev
-print(f"Kendes {nev}! ")
+print(f"Kendes {nev}! {kor} éves vagy!")
