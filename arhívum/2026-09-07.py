7799e64e79694a0ce35a0bbca0d566599ac17085
Kiss-Pető Kende <kiss-peto.kende@petrik.hu>
2026-09-14 12:12:24 +0200

    sorting the files

---
+++ arhívum/2026-09-07.py
@@ -0,0 +1,56 @@
+print("------- 1. feladat --------")
+
+print("hello word!")
+
+
+print("------- 2. feladat --------")
+
+print("isten hozott a python világában!")
+print()
+
+print("\tÜdv: a program")
+print()
+
+print("isten hozott a python világában!\n\n\tÜdv: a program")
+print("""
+isten hozott a python világában!
+
+\tÜdv: a program
+""")
+
+
+print("------- 3. feladat --------")
+
+print('"Ha med tudod álmodni, meg is tudod tenni"', end="\t")
+print("Walt Disney")
+print()
+
+print('"Ha med tudod álmodni, meg is tudod tenni"', end=" - ")
+print("Walt Disney")
+print()
+
+print("Walt Disney:", end="\t")
+print('"Ha med tudod álmodni, meg is tudod tenni"')
+print()
+
+
+print("------- 4. feladat --------")
+
+print("tej", "tej", "tej", "tej", "tej", sep=" ,")
+print("tej", "tej", "tej", "tej", "tej", sep=" | ")
+print("tej", "tej", "tej", "tej", "tej", sep=" *** ")
+print()
+print("------- 5. feladat --------")
+
+print("""
+\" A 'whitespace' vagy 'white space' angol szóöszetétel, jelentése fehér tér.
+
+Az informatikában, elsősorban a
+\tszövegszerkeztésben és
+\tprogramozásban
+használatos kifejezés
+
+Alapvetően azokat a karaktereket értjük alatta, amelyek nem láthatóak a szövegben,
+viszont valamilyen egyedi funkcióval bírnak. Nincs elterjedt magyar kifejezés rá.\"
+\t-- A Wikipédiából, (a szabad enciklopédiából)
+""")
